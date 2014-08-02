5b34ca74a2536bb7d57c273f9041d1a992815d5d
Minh Tu Le <minhtu@labgoo.com>
2014-08-01 19:23:52 -0700

    Added AppsFlyer's conversion data key constants

--- Providers/AppsFlyerProvider.h
+++ Providers/AppsFlyerProvider.h
@@ -13,3 +13,34 @@
 - (instancetype)initWithITunesAppID:(NSString *)iTunesAppID key:(NSString *)key;
 
 @end
+
+// Key names are taken from the following guide
+// http://support.appsflyer.com/entries/69796693-Accessing-AppsFlyer-Attribution-Conversion-Data-from-the-SDK-Deferred-Deep-linking-
+
+// All media sources
+
+extern NSString *const kARAFAttributeKeyAFStatus;
+extern NSString *const kARAFAttributeKeyAFMessage;
+extern NSString *const kARAFAttributeKeyMediaSource;
+extern NSString *const kARAFAttributeKeyCampaignName;
+extern NSString *const kARAFAttributeKeyClickID;
+extern NSString *const kARAFAttributeKeyAFSiteID;
+extern NSString *const kARAFAttributeKeyClickTime;
+extern NSString *const kARAFAttributeKeyInstallTime;
+extern NSString *const kARAFAttributeKeyAgency;
+
+// Facebook
+
+extern NSString *const kARAFAttributeKeyIsFB;
+extern NSString *const kARAFAttributeKeyFBAdGroupName;
+extern NSString *const kARAFAttributeKeyFBAdGroupID;
+extern NSString *const kARAFAttributeKeyFBCampaignID;
+extern NSString *const kARAFAttributeKeyFBAdSetName;
+extern NSString *const kARAFAttributeKeyFBAdSetID;
+extern NSString *const kARAFAttributeKeyFBAdID;
+
+// Valid values
+
+extern NSString *const kARAFAttributeAFStatusOrganic;
+extern NSString *const kARAFAttributeAFStatusNonOrganic;
+extern NSString *const kARAFAttributeAFStatusError;
--- Providers/AppsFlyerProvider.m
+++ Providers/AppsFlyerProvider.m
@@ -40,3 +40,31 @@
 #endif
 
 @end
+
+// All media sources
+
+NSString *const kARAFAttributeKeyAFStatus = @"af_status";
+NSString *const kARAFAttributeKeyAFMessage = @"af_message";
+NSString *const kARAFAttributeKeyMediaSource = @"media_source";
+NSString *const kARAFAttributeKeyCampaignName = @"campaign";
+NSString *const kARAFAttributeKeyClickID = @"clickid";
+NSString *const kARAFAttributeKeyAFSiteID = @"af_siteid";
+NSString *const kARAFAttributeKeyClickTime = @"click_time";
+NSString *const kARAFAttributeKeyInstallTime = @"install_time";
+NSString *const kARAFAttributeKeyAgency = @"agency";
+
+// Facebook
+
+NSString *const kARAFAttributeKeyIsFB = @"is_fb";
+NSString *const kARAFAttributeKeyFBAdGroupName = @"adgroup_name";
+NSString *const kARAFAttributeKeyFBAdGroupID = @"adgroup_id";
+NSString *const kARAFAttributeKeyFBCampaignID = @"campaign_id";
+NSString *const kARAFAttributeKeyFBAdSetName = @"adset_name";
+NSString *const kARAFAttributeKeyFBAdSetID = @"adset_id";
+NSString *const kARAFAttributeKeyFBAdID = @"ad_id";
+
+// Valid values
+
+NSString *const kARAFAttributeAFStatusOrganic = @"Organic";
+NSString *const kARAFAttributeAFStatusNonOrganic = @"Non-organic";
+NSString *const kARAFAttributeAFStatusError = @"Error";
